1a28d5e0c3fca6e9f21afecdb7294f5e44611e5b
brunosoarespaiva <sp.bruno@gmail.com>
2023-10-17 15:44:58 -0300

    Deploy inicial
    20231017 - Versão Inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+# Primeiro programa em python 14/10/2023
+print ("Teste")
+
+n1 = 5
+n2 = 5
+
+Soma = 0
+Subtracao = 0
+Divisao = 0
+Multiplicacao = 0
+
+soma = n1 + n2
+print ('Soma:', soma)
+
+Subtracao = n1 - n2
+print ('Subtracao:', Subtracao)
+
+
+Multiplicacao = n1 * n2
+print ('Multiplicacao:', Multiplicacao)
+
+if (n1 == 0) or (n2 == 0):
+    print ('Dvisao por zero') 
+
+else:  
+    Divisao = n1 / n2
+    print ('Divisao:', Divisao) 
